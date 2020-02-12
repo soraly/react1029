e892811c39a9b024f7e5ec07b304f53e3670cf15
soraly <soraly@163.com>
2020-02-12 22:48:20 +0800

    add

--- todolist211/src/App.js
+++ todolist211/src/App.js
@@ -2,16 +2,66 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function testable(target){
-  target.name = 'xiang';
+
+function getArgs (func) {
+  console.log('function '+func.toString(),'func')
+  // First match everything inside the function argument parens.
+  const args = ('function '+func.toString()).match(/function.*?\(([^)]*)\)/)[1]
+  // Split the arguments string into an array comma delimited.
+  console.log(args,'args')
+  return args
+    .split(',')
+    .map(function (arg) {
+      // Ensure no inline comments are parsed and trim the whitespace.
+      return arg.replace(/\/\*.*\*\//, '').trim()
+    })
+    .filter(function (arg) {
+      // Ensure no undefined values are added.
+      return arg
+    })
 }
 
-@testable
+function testable(target,name, descriptor){
+  const originFunction = descriptor.value;
+
+  const originFunctionArguments = getArgs(originFunction);
+  console.log(originFunctionArguments,'originFunctionArguments')
+  descriptor.value = function () {
+    let newFunctionAruments = []
+    originFunctionArguments.map(argument => {
+      if (argument[0] === argument[0].toUpperCase()) {
+        let bean = void 0
+        if (argument == 'Link') {
+          bean = 'this is link func'
+        } else {
+          bean = 'this is age func'
+        }
+        bean && newFunctionAruments.push(bean)
+      }
+    })
+    console.log(newFunctionAruments,'newFunctionAruments')
+    if (arguments.length) {
+      newFunctionAruments = [...newFunctionAruments, ...Array.from(arguments)]
+    }
+    return originFunction.apply(this, newFunctionAruments)
+  }
+  
+  return descriptor
+
+}
+
 class Lzx {
+  constructor(){
+    this.sayHello('xixi')
+  }
 
+  @testable
+  sayHello(Link,Age,other){
+    console.log(Link,Age,'Link,age');
+    console.log(other,'other')
+  }
 }
-
-console.log(Lzx.name)
+var xiang = new Lzx()
 
 function App() {
   return (
